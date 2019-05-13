2b87bf6339eb5059e5bebd83c321deb09ffc79c8
0lorak <50457699+0lorak@users.noreply.github.com>
2019-05-13 09:31:46 -0500

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -3,11 +3,6 @@
 install.packages("reticulate") # python support in RMarkdown
 install.packages("ggplot2") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
-#install.packages("lattice") # to be able to install markovchain package
-#install.packages("Matrix") # to be able to install markovchain package
-#install.packages("RcppParallel") # to be able to install markovchain package
-#install.packages("matlab") # to be able to install markovchain package
-#install.packages("markovchain") # to be able to work with Markov Chains
 
 ### install bioconductor packages
 # install.packages("BiocManager")
